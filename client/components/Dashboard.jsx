605b7091bbb438fd796ba3d69bbe30aabe99ecdc
James <jamesli0226@gmail.com>
2023-07-03 23:17:46 -0400

    implemented search by zip code

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from './Header';
 import Main from './main';
 import './dashboard.scss';
+import { useState } from 'react';
 
 function Dashboard() {
+
+  const [fetchedData, setFetchedData] = useState([]);
+
   return (
     <div className='dashboard'>
-      <Header />
-      <Main />
+      <Header 
+      fetchedData={fetchedData}
+      setFetchedData={setFetchedData}
+      />
+      <Main 
+      fetchedData={fetchedData}
+      setFetchedData={setFetchedData}
+      />
     </div>
   );
 }
